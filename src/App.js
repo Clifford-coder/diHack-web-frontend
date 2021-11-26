73dbad1fc44d615855d5a9ad9b4c3588f6e54270
Clifford-coder <cliffowusu2727@gmail.com>
2021-11-25 23:38:28 -0800

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from 'react-router-dom';
+import 'tailwindcss/dist/base.min.css';
+import IndexPage from './pages/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let routes = useRoutes([{ path: '/', element: <IndexPage /> }]);
+  return routes;
 }
 
 export default App;
